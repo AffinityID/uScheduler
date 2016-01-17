1e3bf27f68e95760451b56211573ed939930a32f
Lee Lian Hoy <cruz@zyk3.net>
2016-01-18 12:40:01 +1300

    Added update of language files.

--- uScheduler/SchedulerApplication.cs
+++ uScheduler/SchedulerApplication.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Threading;
+using System.Web;
+using System.Xml;
 using log4net;
+using umbraco;
 using umbraco.businesslogic;
 using umbraco.interfaces;
 using uScheduler.Models;
@@ -20,6 +23,14 @@ namespace uScheduler {
         public SchedulerApplication() {
             _log = LogManager.GetLogger(GetType());
             Runner = new ScheduleRunner(ApplicationContext.Current);
+        }
+
+        protected override void ApplicationInitialized(
+            UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext
+            ) {
+            UpdateLocalization("en.xml", "sections", "uScheduler", "uScheduler");
+            UpdateLocalization("en_us.xml", "sections", "uScheduler", "uScheduler");
         }
 
         protected override void ApplicationStarted(
@@ -44,9 +55,23 @@ namespace uScheduler {
             }
         }
 
-//        private void UpdateLocalization() {
-//            var localization = ApplicationContext.Current.Services.LocalizationService;
-//            localization.GetDictionaryItemByKey()
-//        }
+        private void UpdateLocalization(string languageFile, string area, string key, string value) {
+            var umbracoPath = GlobalSettings.Path;
+            var doc = XmlHelper.OpenAsXmlDocument($"{umbracoPath}/config/lang/{languageFile}");
+
+            var areaNode = doc.SelectSingleNode($"//area[@alias='{area}']");
+            if (areaNode == null || areaNode.SelectSingleNode($"key[@alias='{key}']") != null) return;
+
+            var node = areaNode.AppendChild(doc.CreateElement("key"));
+            if (node.Attributes == null)
+                throw new XmlException($"Error creating the key element in {languageFile} -> {area}");
+
+            var attr = node.Attributes.Append(doc.CreateAttribute("alias"));
+            attr.InnerText = key;
+            node.InnerText = value;
+
+            doc.Save(HttpContext.Current.Server.MapPath($"{umbracoPath}/config/lang/{languageFile}"));
+            _log.Info($"Successfully update language file {languageFile} with area: {area}, key: {key}, value: {value}");
+        }
     }
 }
