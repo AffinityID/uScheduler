34b2cf7111bc091cb6d7b6f83629f06cc20495b4
Lee Lian Hoy <cruz@zyk3.net>
2016-01-18 13:24:04 +1300

    Added description.

--- uScheduler/Properties/AssemblyInfo.cs
+++ uScheduler/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("uScheduler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A task scheduling module for Umbraco.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Affinity Id")]
 [assembly: AssemblyProduct("uScheduler")]
